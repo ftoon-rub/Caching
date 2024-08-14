8b2a98add2c36830a8699e55296e7f753ba55d9d
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-08-14 13:23:45 +0300

    add redis controller and edit connection string

---
+++ Caching/Controllers/RedisController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+    }
+}
