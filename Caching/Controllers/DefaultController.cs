7108cfce352986f6929d94cfd3607402dd3159f2
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-08-14 13:23:06 +0300

    add default controller

---
+++ Caching/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caching.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("api is up and running");
+        }
+    }
+}
