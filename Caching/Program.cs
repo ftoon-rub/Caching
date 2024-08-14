43c47b42c7bdf850a31b3f675821676ca91c114b
Ftoon Bin Rbia <fbinrbia@bcare.com.sa>
2024-08-14 08:15:29 +0300

    add appsettingsconfigration class and bind it with appsetting json, add dependency injection registaraton for custom and external services

--- Caching/Program.cs
+++ Caching/Program.cs
@@ -1,6 +1,25 @@
+using Core.Configurations;
+using Infrastructure.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configuration = new ConfigurationBuilder()
+         .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+         .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+         .AddEnvironmentVariables()
+         .Build();
+
 // Add services to the container.
+var config = new AppSettingsConfiguration();
+configuration.Bind(config);
+builder.Services.AddSingleton(config);
+
+builder.Services.AddExternalLibrariesServices(config);
+builder.Services.AddCustomServices(config);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
